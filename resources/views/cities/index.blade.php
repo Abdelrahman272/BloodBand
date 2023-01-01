42b8e9d2827a2cfe9f4d71abec6e2ad335a9215b
Abdelrahman272 <82956280+Abdelrahman272@users.noreply.github.com>
2023-01-01 03:00:25 +0200

    blood banck

--- resources/views/cities/index.blade.php
+++ resources/views/cities/index.blade.php
@@ -18,7 +18,8 @@
                 </div><!-- /.container-fluid -->
             </section>
             <div class="card-header">
-                <a href="{{route('cities.create')}}" class="btn btn-primary btn-md" role="button" aria-disabled="true">Add Cities</a>
+                <a href="{{ route('cities.create') }}" class="btn btn-primary btn-md" role="button" aria-disabled="true">Add
+                    Cities</a>
             </div>
             @include('flash::message')
             <!-- /.card-header -->
@@ -30,16 +31,24 @@
                                 aria-describedby="example1_info">
                                 <thead>
                                     <tr>
-                                        <th class="sorting sorting_asc" tabindex="0" aria-controls="example1"rowspan="1" colspan="1" aria-sort="ascending"aria-label="Rendering engine: activate to sort column descending">
+                                        <th class="sorting sorting_asc" tabindex="0"
+                                            aria-controls="example1"rowspan="1" colspan="1"
+                                            aria-sort="ascending"aria-label="Rendering engine: activate to sort column descending">
                                             Number
                                         </th>
-                                        <th class="sorting sorting_asc" tabindex="0" aria-controls="example1"rowspan="1" colspan="1" aria-sort="ascending"aria-label="Rendering engine: activate to sort column descending">
+                                        <th class="sorting sorting_asc" tabindex="0"
+                                            aria-controls="example1"rowspan="1" colspan="1"
+                                            aria-sort="ascending"aria-label="Rendering engine: activate to sort column descending">
                                             #ID
                                         </th>
-                                        <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1"colspan="1" aria-label="Browser: activate to sort column ascending">
+                                        <th class="sorting" tabindex="0" aria-controls="example1"
+                                            rowspan="1"colspan="1"
+                                            aria-label="Browser: activate to sort column ascending">
                                             City
                                         </th>
-                                        <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1"colspan="1" aria-label="Engine version: activate to sort column ascending">
+                                        <th class="sorting" tabindex="0" aria-controls="example1"
+                                            rowspan="1"colspan="1"
+                                            aria-label="Engine version: activate to sort column ascending">
                                             Governorate
                                         </th>
                                         <th>Actions</th>
@@ -48,22 +57,25 @@
                                 <tbody>
                                     <?php $i = 1; ?>
                                     @foreach ($models as $model)
-                                    <tr>
-                                    <td>{{$i++}}</td>
-                                    <td>{{$model->id}}</td>
-                                    <td>{{$model->name}}</td>
-                                    <td>{{$model->governorate->name}}</td> 
-                                    <td>
-                                        <a href="{{route('cities.edit', $model->id)}}" class="btn btn-primary" tabindex="-1" role="button" aria-disabled="true">Edit</a>
-                                        <form class="btn" action="{{route('cities.destroy', $model->id)}}" method="POST">
-                                            @method('DELETE')
-                                            @csrf
-                                            <button type="submit" class="btn btn-danger btn-md" data-toggle="modal">Delete</button>
-                                        </form>
-                                    </td> 
-                                    </tr>
+                                        <tr>
+                                            <td>{{ $i++ }}</td>
+                                            <td>{{ $model->id }}</td>
+                                            <td>{{ $model->name }}</td>
+                                            <td>{{ $model->governorate->name }}</td>
+                                            <td>
+                                                <a href="{{ route('cities.edit', $model->id) }}" class="btn btn-primary"
+                                                    tabindex="-1" role="button" aria-disabled="true">Edit</a>
+                                                <form class="btn" action="{{ route('cities.destroy', $model->id) }}"
+                                                    method="POST">
+                                                    @method('DELETE')
+                                                    @csrf
+                                                    <button type="submit" class="btn btn-danger btn-md"
+                                                        data-toggle="modal">Delete</button>
+                                                </form>
+                                            </td>
+                                        </tr>
                                     @endforeach
-                                </tbody> 
+                                </tbody>
                             </table>
                         </div>
                     </div>
